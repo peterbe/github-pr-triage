c2cee1ae0e44c3a84cf591d65c1a1078b2fb3730
Peter Bengtsson <mail@peterbe.com>
2014-03-04 15:31:46 -0800

    much better

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ from flask.views import MethodView
 
 
 MEMCACHE_URL = os.environ.get('MEMCACHE_URL', '127.0.0.1:11211').split(',')
-DEBUG = os.environ.get('DEBUG', False) in ('true', '1')
+DEBUG = os.environ.get('DEBUG', False) in ('true', '1', 'y', 'yes')
+GITHUB_OAUTH_TOKEN = os.environ.get('GITHUB_OAUTH_TOKEN')
 
 app = Flask(__name__)
 cache = MemcachedCache(MEMCACHE_URL)
@@ -24,63 +25,77 @@ def index_html():
     return send_file('index.html')
 
 
-class ShasView(MethodView):
-
-    def post(self):
-        deployments = []
-        for each in request.json:
-            name = each['name']
-            url = each['url']
-            if '?' in url:
-                url += '&'
-            else:
-                url += '?'
-            url += 'cachescramble=%s' % time.time()
-            content = urllib.urlopen(url).read().strip()
-            if not 7 <= len(content) <= 40:
-                # doesn't appear to be a git sha
-                error = (
-                    "Doesn't look like a sha\n (%s) on %s" %
-                    (content, each['url'])
-                )
-                return make_response(jsonify({'error': error,}))
-            deployments.append({
-                'name': name,
-                'sha': content,
-                'bugs': []
-            })
-        response = make_response(jsonify({'deployments': deployments}))
-        return response
-
-class PullsView(MethodView):
-
-    def get(self, owner, repo):
-        url = 'https://api.github.com'
-        url = urlformat(owner=owner, repo=repo)
-        print url
-
-
 class ProxyView(MethodView):
 
-    expires = 60 * 60
+    short_expires = 60 * 10  # when we serve straight from memcache
+    long_expires = 60 * 60 * 24  # store long term
+
+    def _attach_auth(self, headers):
+        if GITHUB_OAUTH_TOKEN:
+            headers['Authorization'] = 'token %s' % GITHUB_OAUTH_TOKEN
 
     def get(self, path):
+        if '://' in path:
+            assert path.startswith(self.base)
+            path = path.replace(self.base, '')
         path = '%s?%s' % (path, request.query_string)
         key = self.prefix + hashlib.md5(path).hexdigest()
-        value = cache.get(key)
-        #value = None
+        short_key = 'short-' + key
+        long_key = 'long-' + key
+        value = cache.get(short_key)
         if value:
             value = json.loads(value)
         else:
-            print "LOADING", self.base + path
-            response = requests.get(self.base + path)
-            assert response.status_code == 200, response.status_code
-            value = response.json()
-            cache.set(key, json.dumps(value), self.expires)
-        #print type(value)
-        response = make_response(json.dumps(value))
-        response.headers['content-type'] = 'application/json'
-        return response
+            # do we have it in long-term memory? If so, do conditional get
+            value = cache.get(long_key)
+            if value:
+                value = json.loads(value)
+                # but is it out of date?
+                # print "We have only a long-term storage of this"
+                if value.get('_etag'):
+                    headers = {'If-None-Match': value['_etag']}
+                    self._attach_auth(headers)
+                    # print path
+                    # print headers
+                    print "CONDITIONAL GET", self.base + path
+                    response = requests.get(self.base + path, headers=headers)
+                    if response.status_code == 304:
+                        # it's still fresh!
+                        cache.set(short_key, json.dumps(value), self.short_expires)
+                        value['_ratelimit_limit'] = response.headers.get('X-RateLimit-Limit')
+                        value['_ratelimit_remaining'] = response.headers.get('X-RateLimit-Remaining')
+                    else:
+                        value = None
+                else:
+                    # it's too old and we can't do a conditional get
+                    value = None
+
+            if not value:
+                print "GET", self.base + path
+                headers = {}
+                self._attach_auth(headers)
+                response = requests.get(self.base + path, headers=headers)
+
+                assert response.status_code == 200, response.status_code
+
+                value = response.json()
+                if not isinstance(value, dict):
+                    # if the JSON response is a list or something we can't
+                    # attach extra stuff to it
+                    value = {'_data': value}
+                cache.set(short_key, json.dumps(value), self.short_expires)
+
+                # we only need these for the long-storage stuff
+                value['_etag'] = response.headers.get('ETag')
+                #value['_last_modified'] = response.headers.get('Last-Modified')
+                cache.set(long_key, json.dumps(value), self.long_expires)
+
+                # these values aren't worth storing in the cache but
+                # useful to return as part of the response
+                value['_ratelimit_limit'] = response.headers.get('X-RateLimit-Limit')
+                value['_ratelimit_remaining'] = response.headers.get('X-RateLimit-Remaining')
+
+        return make_response(jsonify(value))
 
 
 class GithubProxyView(ProxyView):
--- static/app.js
+++ static/app.js
@@ -15,31 +15,92 @@ angular.module('triage', ['ngRoute', 'angularMoment'])
     ;
 }])
 
-.controller('AppController', function($scope, $http, $location) {
-    $scope.pulls = [];
+.factory('ratelimit', function() {
+    var ratelimit = {};
+    var service = {};
 
+    service.update = function(limit, remaining) {
+        ratelimit.limit = limit;
+        ratelimit.remaining = remaining;
+    };
+    service.get = function() {
+        return ratelimit;
+    };
+
+    return service;
+})
+
+.controller('AppController', function($scope, $http, $location, ratelimit) {
+    $scope.ratelimit = ratelimit.get;
 })
 
 .controller('FormController', function($scope, $location) {
     $scope.submitForm = function() {
         $location.path('/' + this.owner + '/' + this.repo);
-        //loadPulls(this.owner, this.repo);
     };
 })
 
-.controller('PullsController', function($scope, $http, $routeParams) {
+.controller('PullsController', function($scope, $http, $routeParams, ratelimit) {
 
     $scope.owner = $routeParams.owner;
     $scope.repo = $routeParams.repo;
     $scope.bugs = {};
-    console.log($scope.owner, $scope.repo);
+
+    $scope.toggleExpandPull = function(pull) {
+        if (!pull._expanded) {
+            pull._events = $scope.getEvents(pull);
+        }
+        pull._expanded = !pull._expanded;
+    };
+
+    function truncate(s, l) {
+        if (s.length > l) {
+            return s.substring(0, l).trim() + '...';
+        }
+        return s;
+    }
+
+    $scope.getEvents = function(pull) {
+        var events = [];
+
+        _.each(pull._commits || [], function(commit) {
+            // console.dir(commit);
+            events.push({
+                _type: 'commit',
+                _url: commit.html_url,
+                _summary: truncate(commit.commit.message, 80),
+                _date: commit.commit.author.date
+            });
+        });
+        _.each(pull._statuses || [], function(status) {
+            //console.dir(status);
+            events.push({
+                _type: 'status-' + status.state,
+                _url: status.target_url,
+                _summary: status.description,
+                _date: status.created_at
+            });
+        });
+        _.each(pull._comments || [], function(comment) {
+            //console.dir(comment);
+            events.push({
+                _type: 'comment',
+                _summary: '(by @' + comment.user.login +') ' + truncate(comment.body, 80),
+                _url: comment.html_url,
+                _date: comment.created_at
+            });
+        });
+        //console.dir(events);
+        //return [];
+        return events;
+    };
 
     $scope.formatDate = function(date) {
         return moment(date).format('ddd, MMM D, YYYY, h:mma UTCZZ');
     };
 
     function findBugNumbers(title) {
-        var re = /\b[0-9]{4,10}\b/;
+        var re = /\b[0-9]{6,10}\b/;
         var bugs = [];
         _.each(re.exec(title), function(bug) {
             bugs.push(bug);
@@ -69,22 +130,116 @@ angular.module('triage', ['ngRoute', 'angularMoment'])
         });
         return users;
     };
+    $scope.getStatuses = function(pull) {
+        return pull._statuses || [];
+    };
 
-    function loadComments(owner, repo, number) {
+    $scope.countComments = function(pull) {
+        return pull._comments && pull._comments.length || 0;
+    };
+
+    $scope.isMergeable = function(pull) {
+        return !!pull.merge_commit_sha;
+    };
+
+    $scope.hasStatuses = function(pull) {
+        return !!pull._statuses;
+    };
+
+    $scope.isSuccessfulLastStatus = function(pull) {
+        var statuses = pull._statuses || [];
+        var last = statuses[0];  // confusing, I know
+        return last.state === 'success';
+    };
+
+    function loadComments(pull) {
         $http
-            .get('/githubproxy/repos/' + owner + '/' + repo + '/pulls/' + number + '/comments')
-            .success(function(data) {
-                _.each($scope.pulls, function(pull) {
-                    if (pull.number === number) {
-                        pull._comments = data;
-                    }
-                });
-            })
-            .error(function(data, status) {
-                console.warn(data, status);
-            });
-
+        .get('/githubproxy/' + pull.comments_url)
+        .success(function(data) {
+            if (data._ratelimit_limit) {
+                ratelimit.update(data._ratelimit_limit, data._ratelimit_remaining);
+            }
+            pull._comments = data._data;
+            if (pull._comments.length) {
+                pull._last_comment = pull._comments[pull._comments.length - 1];
+                setLastActor(pull);
+            }
+        })
+        .error(function(data, status) {
+            console.warn(data, status);
+        });
     }
+
+    function loadStatuses(pull) {
+        $http
+        .get('/githubproxy/' + pull.statuses_url)
+        .success(function(data) {
+            if (data._ratelimit_limit) {
+                ratelimit.update(data._ratelimit_limit, data._ratelimit_remaining);
+            }
+            pull._statuses = data._data;
+        })
+        .error(function(data, status) {
+            console.warn(data, status);
+        });
+    }
+
+    function loadCommits(pull) {
+        $http
+        .get('/githubproxy/' + pull.commits_url)
+        .success(function(data) {
+            if (data._ratelimit_limit) {
+                ratelimit.update(data._ratelimit_limit, data._ratelimit_remaining);
+            }
+            pull._commits = data._data;
+            if (pull._commits.length > 1) {
+                pull._last_commit = pull._commits[pull._commits.length - 1];
+                setLastActor(pull);
+            }
+            // console.log('COMMITS');
+            // console.dir(data);
+        })
+        .error(function(data, status) {
+            console.warn(data, status);
+        });
+    }
+
+    function setLastActor(pull) {
+        if (pull._last_commit && pull._last_comment) {
+            // but who was first?!
+            if (pull._last_commit.commit.date > pull._last_comment.created_at) {
+                pull._last_actor = {
+                    user: pull._last_commit.author,
+                    type: "commit",
+                    url: pull._last_commit.html_url
+                };
+            } else {
+                pull._last_actor = {
+                    user: pull._last_comment.user,
+                    type: "comment",
+                    url: pull._last_comment.html_url
+                };
+            }
+        } else if (pull._last_commit) {
+            pull._last_actor = {
+                user: pull._last_commit.author,
+                type: "commit",
+                url: pull._last_commit.html_url
+            };
+        } else if (pull._last_comment) {
+            pull._last_actor = {
+                user: pull._last_comment.user,
+                type: "comment",
+                url: pull._last_comment.html_url
+            };
+        } else {
+            pull._last_actor = {
+                user: pull.user,
+                type: "creation",
+                url: pull.html_url
+            };
+        }
+    };
 
     function loadPulls(owner, repo) {
         $http
@@ -93,68 +248,43 @@ angular.module('triage', ['ngRoute', 'angularMoment'])
             //console.dir(data);
             var pulls = [];
             var bugs = [];
-            _.each(data, function(pull) {
+            if (data._ratelimit_limit) {
+                ratelimit.update(data._ratelimit_limit, data._ratelimit_remaining);
+            }
+            // console.dir($scope.ratelimit);
+            _.each(data._data, function(pull) {
                 pull._bugs = findBugNumbers(pull.title);
                 bugs = _.union(bugs, pull._bugs);
+                pull._last_user = pull.user;
+                pull._last_user_time = pull.created_at;
+                setLastActor(pull);
                 pulls.push(pull);
-                loadComments(owner, repo, pull.number);
+                //console.log('URL1', pull.comments_url);
+                loadComments(pull);
+                loadStatuses(pull);
+                loadCommits(pull);
             });
             $scope.pulls = pulls;
-            console.log('ALL BUGS', bugs);
+            // console.log('ALL BUGS', bugs);
             $http
-                .get('/bugzillaproxy/bug?id=' + bugs.join(',') + '&include_fields=summary,id,status,resolution,is_open')
-                .success(function(data, status) {
-                    console.log(data);
-                    var bugs = {};
-                    _.each(data.bugs, function(bug) {
-                        bugs[bug.id] = bug;
-                    });
-                    $scope.bugs = bugs;
-                })
-                .error(function(data, status) {
-                    console.warn(data, status);
+            .get('/bugzillaproxy/bug?id=' + bugs.join(',') + '&include_fields=summary,id,status,resolution,is_open')
+            .success(function(data, status) {
+                // console.log(data);
+                var bugs = {};
+                _.each(data.bugs, function(bug) {
+                    bugs[bug.id] = bug;
                 });
+                $scope.bugs = bugs;
+            })
+            .error(function(data, status) {
+                console.warn(data, status);
+            });
         })
         .error(function(data, status) {
         });
     }
     loadPulls($scope.owner, $scope.repo);
 
-
 })
 
-// .controller('PullController', function($scope, $http, $routeParams) {
-//     $scope.pulls = [];
-//     console.log('In PullController');
-//     function loadPulls(owner, repo) {
-//         $http
-//         .get('/proxy/repos/' + owner + '/' + repo + '/pulls?state=open')
-//         .success(function(data, status, headers) {
-//             console.dir(data);
-//             $scope.pulls = data;
-//         })
-//         .error(function(data, status) {
-//         });
-//     }
-//
-//     $scope.submitForm = function() {
-//         console.log(this.owner, this.repo);
-//     };
-//     console.dir($routeParams);
-// console.log($routeParams.owner);
-// console.log($routeParams.repo);
-//    // console.log($location.search())
-//    // console.log(($location.search()).owner);
-//    // console.log(($location.search()).repo);
-//
-// })
-//
-// .controller('StartController', function($scope, $location) {
-//     console.log('In StartController');
-//     $scope.owner = 'mozilla';
-//     $scope.repo = 'socorro';//TEMP
-//     $scope.submitForm = function() {
-//         $location.path('/' + this.owner + '/' + this.repo);
-//     };
-// })
 ;
